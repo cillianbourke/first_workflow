edf21fe83aa3e73baaaab3c92fb78d9659d5212e
cillianbourke <cillian.bourke@accenture.com>
2026-04-22 10:27:00 +0100

    Update new_bucket.tf

--- lesson-4-demos/terraform-demo/new_bucket.tf
+++ lesson-4-demos/terraform-demo/new_bucket.tf
@@ -6,7 +6,8 @@ provider "aws" {
 
 # Create an S3 bucket
 resource "aws_s3_bucket" "udabucket" {
-  bucket = "cicd-terraform-demo-bucket20213"
+  bucket = "cicd-terraform-demo-bucket-cillian-20213"
+
 
   tags = {
     Name        = "CICD test bucket"
